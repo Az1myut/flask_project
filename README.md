32aa530602a794321d6a19ca1766ef2b17ea7e65
Daniel Jobst <daniel.jobst@oth-regensburg.de>
2025-05-22 19:04:03 +0200

    Stand 22.05.2025

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # AGWI Beispielprojekt
 
-Stand 15.05.2025
+Stand 22.05.2025
+
+## Cascading Style Sheets (CSS)
+
+Mit Hilfe der erstellten [css-Datei](static/css/style.css) werden Layout-Informationen (Farben, Schrift, Rahmen, Position, ...)
+den Elementen in HTML zugeordnet.
+
+Grundsätzlich im Format `<SELECTOR> { <PROPERTY>:<VALUE>; ... }` mit folgenden drei genutzten *Selektoren* (Beispiele):
+- `h1 { color:rgb(30,45,129); backgroud-color:blue; font-family:Arial; }` --> legt das Layout für alle `<h1>`-Elemente fest
+- `#xyz { ... }` --> legt das Layout für das Element `<div id="xyz">...</div>` fest (und für die enthaltenen Unter-Elemente)
+- `.abc { ... }` --> legt das Layout für Elemente fest, die der Klasse `abc` zugeordnet werden, z. B. `<p class="abc">`
+
+## Frontend-Toolkit 'Bootstrap'
+
+Das Frontend-Toolkit [Bootstrap](https://www.getbootstrap.com) definiert ein eine große Menge an CSS-Klassen und gibt viele Beispiele, 
+wie diese genutzt werden können. Die HTML-Beispiele in [Docs](https://getbootstrap.com/docs/5.3/getting-started/introduction/)
+wie *Tables*, *Forms*, *Cards* u. v. m. können einfach für die eigenen HTML-Templates genutzt und angepasst werden.
+
+Einzige Voraussetzung ist, dass die CSS-Datei von Bootstrap im HTML-Header des eigenen HTML-Templates eingebunden wurde.
+Dies geschieht mit dem Element `<link rel="stylesheet" href="...">` im HTML-Element `<head>`.
+In diesem Beispielprojekt z. B. in [base.html](templates/base.html) zu finden.
+
+## Template-Engine
+
+Mit `from flask import render_template` kann die Methode `render_template("mein_template.html", ...)` genutzt werden.
+Hierfür werden HTML-Dateien im Ordner [/templates](templates) als Templates erstellt, deren Inhalt dann von der Methode
+geladen und zurückgegeben wird.
+
+Für variable Anteile im Template werden diese mit `{{ variablen_name }}` definiert.
+Fallunterscheidungen und Wiederholungen (Schleifen) sind im Template ebenfalls möglich mit `{% if ... %}...{% endif %}` 
+und `{% for ...%}...{% endfor %}`. Siehe dazu die Beispiele in den Templates!
 
 ## Besprochene HTML-Elemente
 
@@ -22,6 +52,7 @@ Stand 15.05.2025
 
 ## Besprochene Python- und Flask-Features
 
+- Templates mit render_template()-Methode
 - Methodendefinition mit `def` und Routendefinition (Link) zur Methode mit `@app.route`
 - Variablen, Listen, Maps
 - foreach-Schleife `for ... in ...`
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,92 +6,29 @@ app = Flask(__name__)
 @app.route('/')
 def startpage():  # put application's code here
     print("Ja, es wurde wirklich diese Methode aufgerufen!")
-    antwort = """
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>AGWI 15.5.25</title>
-            </head>
-            <body>
 
-                <h1>AGWI ist super</h1>
-                <h2>Erster Grund</h2>
-                <p>Ist nicht kompliziert.</p>
-                <p>Zweiter Absatz. </p>
-                <h2>Zweiter Grund</h2>
-                <p>Weil halt! 
-                    <a href="/test0815">Hier</a> 
-                   geht es zur Testseite
-                </p>
-                <p>Und 
-                    <a href="http://www.zalando.de">Zalando</a> 
-                    ist auch aufrufbar.
-                </p>
-
-                <p>
-                    <a href="http://de.wikipedia.org/wiki/Katzen">
-                        <img src="https://wallpapers.com/images/hd/cute-cat-pictures-cd92rtzsrel4rtwm.jpg" width="150px"></img>
-                    </a>
-
-                    <img src="static/tree.jpg"></img>
-                </p>
-
-                <p>
-                    <a href="/produkte">Hier geht es zu den Produkten</a>
-                </p>
-
-                <p>
-                    <a href="/user">Hier neu registrieren</a>
-                </p>
-
-            </body>
-        </html>
-    """
-    return antwort
+    return render_template('start.html')
 
 
 @app.route('/user')
 def user_form():
-    html = """
-    <form action="/do_user_registrierung" method="POST">
-        <label for="un">Username:</label><br>
-        <input type="text" id="un" name="username_" value="Bitte geben Sie Ihren Namen ein"><br>
-
-        <label for="pw">Passwort:</label><br>
-        <input type="password" id="pw" name="password_"><br><br>
-
-        <input type="submit" value="Jetzt registrieren">
-    </form> 
-    """
-
-    return html
+    return render_template('user_formular.html')
 
 
 @app.route('/do_user_registrierung', methods=['POST'])
 def user_anlegen():
     username = request.form.get("username_")  # alternative Syntax: request.form['username_']
     password = request.form.get("password_")
-    return f"User {username} angelegt (Passwort:{password})"
+    return render_template('user_angelegt.html', user=username, pw=password)
 
 
 @app.route("/produkte")
 def produkte_anzeigen():
-    html = """
-    <h1>Produkte</h1>
-      <ul>
-      """
-
-    html_end = """
-    </ul>
-    """
 
     # z. B. aus Datenbank die Produkte/Kategorien/... holen
     produkt_ids = (1, 2, 3, 77, 432, 9, 200, 89756434, 45637)
-    for id in produkt_ids:
-        html += f"<li><a href=\"/produkte/{id}\">Produkt {id}</a></li>"
 
-    html += html_end
-    return html
+    return render_template('produkt_liste.html', products=produkt_ids)
 
 
 @app.route("/produkte/<produkt_id>")
